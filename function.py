32f3202405b8a92c8ba2606718b396784d7c1ff9
cuckoo <firecode911@protonmail.com>
2019-12-09 16:37:51 +0800

    add

--- function.py
+++ function.py
@@ -1,5 +1,16 @@
-def wash():
+def wash(dry):
 	print('加水')
 	print('加洗衣液')
 	print('旋转')
-wash()
+	if dry:
+		print('烘干')
+wash(False)
+print('-----------')
+print('^ == ^')
+wash(True)
+print('==========')
+def add(x, y):
+	print(x + y)
+add(3, 4)
+add(123, 100)
+
